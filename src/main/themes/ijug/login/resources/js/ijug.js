260e1402b9dd533e6b2ab9a701a26876a28b9474
Niko Köbler <niko@n-k.de>
2025-07-26 20:26:46 +0200

    add persistent IdP selection using localStorage
    Signed-off-by: Niko Köbler <niko@n-k.de>

--- src/main/themes/ijug/login/resources/js/ijug.js
+++ src/main/themes/ijug/login/resources/js/ijug.js
@@ -1,11 +1,30 @@
+const LOCAL_STORAGE_KEY = 'ijug_idp';
+
 function selectIdP(e) {
-	const option = document.querySelector('option[value="' + e.value + '"]');
+	const alias = e.value;
+	window.localStorage.setItem(LOCAL_STORAGE_KEY, alias);
+	selectIdPFromAlias(alias);
+}
+
+function selectIdPFromAlias(alias) {
+	const option = document.querySelector('option[value="' + alias + '"]');
 	const url = option.dataset.url;
 	const target = document.getElementById('btnLogin');
 	target.href = url;
-	if (url) {
+	if (alias) {
 		target.classList.remove('pf-m-disabled');
 	} else {
 		target.classList.add('pf-m-disabled');
 	}
 }
+
+function selectIdPFromLocalStorage() {
+	const storedAlias = window.localStorage.getItem(LOCAL_STORAGE_KEY);
+	if (storedAlias) {
+		selectIdPFromAlias(storedAlias);
+		const select = document.getElementById('login-select-toggle');
+		select.value = storedAlias;
+	}
+}
+
+document.addEventListener('DOMContentLoaded', selectIdPFromLocalStorage);
