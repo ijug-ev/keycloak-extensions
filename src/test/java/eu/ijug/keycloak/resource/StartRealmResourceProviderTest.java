1828bda89213c0dd9969b8b23ac49e220d55f2b8
Niko Köbler <niko@n-k.de>
2026-04-26 21:02:52 +0200

    Add welcome theme redirect test and mount ijug theme in testcontainer
    Mount the local ijug welcome theme into the container so the test
    verifies the actual theme is served (not the Keycloak default stub).
    Add a test for the meta http-equiv="refresh" redirect to /realms/ijug/start/
    and extend the start endpoint test to assert the stub text is absent.
    
    Signed-off-by: Niko Köbler <niko@n-k.de>

--- src/test/java/eu/ijug/keycloak/resource/StartRealmResourceProviderTest.java
+++ src/test/java/eu/ijug/keycloak/resource/StartRealmResourceProviderTest.java
@@ -3,22 +3,24 @@ package eu.ijug.keycloak.resource;
 import dasniko.testcontainers.keycloak.KeycloakContainer;
 import de.keycloak.TestBase;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.testcontainers.images.PullPolicy;
+import org.testcontainers.containers.BindMode;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.time.Duration;
-
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
 
 @Testcontainers
 public class StartRealmResourceProviderTest extends TestBase {
 
 	@Container
 	private static final KeycloakContainer keycloak = new KeycloakContainer("quay.io/keycloak/keycloak:latest")
-		.withImagePullPolicy(PullPolicy.ageBased(Duration.ofDays(1)))
+		.withFileSystemBind("src/main/themes/ijug", "/opt/keycloak/themes/ijug", BindMode.READ_ONLY)
+		.withEnv("KC_SPI_THEME__WELCOME_THEME", "ijug")
 		.withDefaultProviderClasses();
 
 	@BeforeAll
@@ -27,12 +29,22 @@ public class StartRealmResourceProviderTest extends TestBase {
 		initTestRealm(keycloak, "test", null, null);
 	}
 
+	@Test
+	void testRootRedirectsToIjugStart() {
+		given()
+			.get(keycloak.getAuthServerUrl() + "/")
+			.then().statusCode(200)
+			.body(containsString("http-equiv=\"refresh\""))
+			.body(containsString("url=/realms/ijug/start/"));
+	}
+
 	@ParameterizedTest
 	@CsvSource( { "ijug,200", "test,404" })
 	void testStart(String realm, int expectedStatusCode) {
 		given()
 			.get(keycloak.getAuthServerUrl() + "/realms/" + realm + "/start")
-			.then().statusCode(expectedStatusCode);
+			.then().statusCode(expectedStatusCode)
+			.body(not(containsString("This is just a development stub and must be replaced in production.")));
 	}
 
 }
